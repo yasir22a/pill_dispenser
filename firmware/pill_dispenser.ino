56c647a74f6ff6c7588dcddfc787c7d8f75fb8ca
Mohd Yasir <yasiraugust38@gmail.com>
2025-12-16 00:50:25 +0530

    Add header comments and metadata to pill_dispenser.ino

--- firmware/pill_dispenser.ino
+++ firmware/pill_dispenser.ino
@@ -1,5 +1,14 @@
 /**************************************************************************
+
  * Pill Dispenser
+
+ /*
+ Smart Pill Dispenser
+ Author: Mohd Yasir
+ License: MIT
+ GitHub: https://github.com/yasir22a/pill_dispenser
+*/
+
  **************************************************************************/
 
 #include <Arduino.h>
